ee66566e291397ad89b97982a175b20f574ae23d
Yansh <www.shreyanshkumarsahu100@gmail.com>
2024-12-22 18:53:27 +0530

    update_file2

--- client/src/pages/payment/sendOrderRequest.js
+++ client/src/pages/payment/sendOrderRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { updateCartDetails } from "../../redux/features/cart/actions";
 import { setToast } from "../../utils/extraFunctions";
-import { removeItem } from "../../utils/localstorage";
+import { removeItem } from "../../utils/localStorage";
 
 
 export const sendOrderRequest = async (shippingDetails, orderId, response, orderSummary, cartProducts, token, toast, dispatch, navigate) => {
